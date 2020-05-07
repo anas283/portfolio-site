9a5a7494f1f314977725d3be4073a8c00017e364
Anas283 <muhammadanasbinadam@gmail.com>
2020-05-07 21:31:01 +0800

    reduce font size

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,7 @@ class Home extends Component {
     return (
       <div className="flex justify-center bg-gray-100 max-w-4xl mx-auto max-h-full md:h-screen xl:h-full">
         <div className="w-full px-6 mt-24 md:mt-32">
-          <h1 className="text-6xl font-bold md:text-6xl">Hello, I'm Anas</h1>
+          <h1 className="text-5xl font-bold md:text-6xl">Hello, I'm Anas</h1>
           <p className="w-full font-hairline text-gray-700 text-md mt-6 md:w-4/5 md:text-lg">
             I'm a freelance front-end developer, my expertise is in the area of
             responsive design. With every line of code, I strive to make the web
