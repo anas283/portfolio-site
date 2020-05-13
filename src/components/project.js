cdfeec0dfde9b4d85ffdf03d81006a946625a83a
Anas283 <muhammadanasbinadam@gmail.com>
2020-05-14 02:35:10 +0800

    add link to href

--- src/components/project.js
+++ src/components/project.js
@@ -9,14 +9,26 @@ class Project extends Component {
       <div className="p-4 max-w-4xl mx-auto h-screen">
         <h1 className="mt-4 text-lg font-semibold">My latest projects</h1>
         <div className="md:flex justify-start">
-          <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2">
-            <h5 className="-mt-2 text-white">Landing page</h5>
-            <img className="mt-2 rounded" src={LandingPage} alt="" />
-          </div>
-          <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:ml-2">
-            <h5 className="-mt-2 text-white">Dashboard</h5>
-            <img className="mt-2 rounded" src={Dashboard} alt="" />
-          </div>
+          <a
+            href="https://magnes-page.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2">
+              <h5 className="-mt-2 text-white">Landing page</h5>
+              <img className="mt-2 rounded" src={LandingPage} alt="" />
+            </div>
+          </a>
+          <a
+            href="https://magnes-page.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:ml-2">
+              <h5 className="-mt-2 text-white">Dashboard</h5>
+              <img className="mt-2 rounded" src={Dashboard} alt="" />
+            </div>
+          </a>
         </div>
       </div>
     );
