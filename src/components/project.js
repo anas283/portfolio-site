0ea0263bec6c56f4ff34209dfac08a90358c1ec7
Anas283 <muhammadanasbinadam@gmail.com>
2020-05-16 22:13:59 +0800

    add github link

--- src/components/project.js
+++ src/components/project.js
@@ -6,8 +6,21 @@ class Project extends Component {
   render() {
     return (
       <div className="p-4 max-w-4xl mx-auto h-screen">
-        <h1 className="mt-4 text-lg font-semibold">My latest project</h1>
-        <div className="flex flex-col md:justify-start">
+        <h1 className="mt-4 text-lg font-bold md:text-2xl">
+          My latest project
+        </h1>
+        <p className="mt-2">
+          All source code are available on{" "}
+          <a
+            href="https://github.com/anas283"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-teal-500 underline hover:text-teal-600"
+          >
+            GitHub
+          </a>
+        </p>
+        <div className="flex flex-col mt-4 md:justify-start">
           <a
             href="https://magnes-page.netlify.app/"
             target="_blank"
