503e9d92b0cb7b2b3a9eccfbb548803f871398e1
Anas283 <muhammadanasbinadam@gmail.com>
2020-05-18 08:58:35 +0800

    initial commit

--- src/components/project.js
+++ src/components/project.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import LandingPage from "../images/landing-page.png";
+import Dashboard from "../images/dashboard.png";
 
 class Project extends Component {
   state = {};
@@ -20,16 +21,39 @@ class Project extends Component {
             GitHub
           </a>
         </p>
-        <div className="flex flex-col mt-4 md:justify-start">
-          <a
-            href="https://magnes-page.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2"
-          >
-            <h5 className="-mt-2 text-white">Landing page</h5>
-            <img className="mt-2 rounded" src={LandingPage} alt="" />
-          </a>
+        <div className="flex flex-col mt-4 md:justify-start bg-gray-300">
+          <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2">
+            <a
+              href="https://magnes-page.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              // className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2"
+            >
+              <h5 className="-mt-2 text-white">Landing page</h5>
+              <img className="mt-2 rounded" src={LandingPage} alt="" />
+            </a>
+          </div>
+
+          <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2">
+            <a
+              href="https://magnes-page.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              // className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2"
+            >
+              <h5 className="-mt-2 text-white">Dashboard</h5>
+              <img className="mt-2 rounded" src={Dashboard} alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:items-start">
+          <div className="w-1/2">
+            <h1>Test 1</h1>
+          </div>
+          <div className="w-1/2">
+            <h1>Test 2</h1>
+          </div>
         </div>
       </div>
     );
