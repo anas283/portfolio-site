7ca1216608b002dc446b5ef3814b4b3e3f85dfa7
Anas283 <muhammadanasbinadam@gmail.com>
2020-05-18 09:05:38 +0800

    fix some bugs

--- src/components/project.js
+++ src/components/project.js
@@ -21,7 +21,7 @@ class Project extends Component {
             GitHub
           </a>
         </p>
-        <div className="flex flex-col mt-4 md:justify-start bg-gray-300">
+        <div className="flex flex-col mt-4 md:justify-start">
           <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2">
             <a
               href="https://magnes-page.netlify.app/"
@@ -34,7 +34,7 @@ class Project extends Component {
             </a>
           </div>
 
-          <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2">
+          {/* <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2">
             <a
               href="https://magnes-page.netlify.app/"
               target="_blank"
@@ -44,16 +44,7 @@ class Project extends Component {
               <h5 className="-mt-2 text-white">Dashboard</h5>
               <img className="mt-2 rounded" src={Dashboard} alt="" />
             </a>
-          </div>
-        </div>
-
-        <div className="flex flex-col md:items-start">
-          <div className="w-1/2">
-            <h1>Test 1</h1>
-          </div>
-          <div className="w-1/2">
-            <h1>Test 2</h1>
-          </div>
+          </div> */}
         </div>
       </div>
     );
