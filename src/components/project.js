169b992d18813ba7ec8557d958e531b14110fe21
Anas283 <muhammadanasbinadam@gmail.com>
2020-05-18 09:57:53 +0800

    add coming soon

--- src/components/project.js
+++ src/components/project.js
@@ -21,30 +21,27 @@ class Project extends Component {
             GitHub
           </a>
         </p>
-        <div className="flex flex-col mt-4 md:justify-start">
+        <div className="flex flex-col mt-4 md:flex-row">
           <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2">
             <a
               href="https://magnes-page.netlify.app/"
               target="_blank"
               rel="noopener noreferrer"
-              // className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2"
             >
               <h5 className="-mt-2 text-white">Landing page</h5>
               <img className="mt-2 rounded" src={LandingPage} alt="" />
             </a>
           </div>
 
-          {/* <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2">
-            <a
-              href="https://magnes-page.netlify.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-              // className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md md:w-1/2 md:mr-2"
-            >
-              <h5 className="-mt-2 text-white">Dashboard</h5>
-              <img className="mt-2 rounded" src={Dashboard} alt="" />
-            </a>
-          </div> */}
+          <div className="bg-gray-900 h-full p-4 mt-4 text-center rounded shadow-md relative md:w-1/2 md:mr-2">
+            <div className="w-full h-16 mt-16 p-0 mt-0 md:mt-20 -ml-4 absolute">
+              <h1 className="text-white mt-6 text-lg font-medium md:mt-6">
+                Coming soon
+              </h1>
+            </div>
+            <h5 className="-mt-2 text-white opacity-25">Dashboard</h5>
+            <img className="mt-2 rounded opacity-25" src={Dashboard} alt="" />
+          </div>
         </div>
       </div>
     );
